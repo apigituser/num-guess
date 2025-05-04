7fd838a08076b622b77f5ecd8fee21acbbffee01
apigituser <responsemail850@gmail.com>
2025-05-04 22:53:33 +0530

    added a timer

--- num-guess.py
+++ num-guess.py
@@ -1,4 +1,20 @@
 import random
+import datetime
+
+def checkGuess():
+    startTime = datetime.datetime.now()
+    for attempts in range(1, chances+1):
+        guess = int(input("Enter your guess -> "))
+        if guess == answer:
+            endTime = datetime.datetime.now() - startTime
+            print(f"Congratulations! You guessed the correct number in {attempts} attempts")
+            print(f"And it only took you {endTime.seconds} seconds!")
+            break
+        elif guess > answer:
+            print(f"Wrong! The number is smaller than {guess}")
+        else:
+            print(f"Wrong! The number is greater than {guess}")
+    
 
 if __name__ == '__main__':
     print('''
@@ -17,20 +33,17 @@ Please select the difficulty level:
 
     print(f"\nGreat! You have selected the {level[choice]} difficulty level.")
     print("Let's start the game!\n")
+    play = 'yes'
 
-    answer = random.randint(1,100)
 
     match choice:
         case 1: chances = 10
         case 2: chances = 5
         case 3: chances = 3
-    
-    for attempts in range(1, chances+1):
-        guess = int(input("Enter your guess -> "))
-        if guess == answer:
-            print(f"Congratulations! You guessed the correct number in {attempts} attempts")
-            break
-        elif guess > answer:
-            print(f"Wrong! The number is smaller than {guess}")
-        else:
-            print(f"Wrong! The number is greater than {guess}")
+
+    while(play == 'yes'):
+        answer = random.randint(1,100)
+        checkGuess()
+        play = input("Do you wish to try again? (yes/no): ").lower()
+    print("Thank you for playing the game!")
+    print("See you later!")
